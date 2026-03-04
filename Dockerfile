072f6d01d3399f94d5d3a85b6da96c43bf1d767b
Sathiya priya <gsathiya91@gmail.com>
2026-03-04 19:09:16 +0530

    Dockerfile changes for DevOps build project

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,6 @@ COPY build/ /usr/share/nginx/html
 # copy custom nginx configuration
 COPY nginx.conf /etc/nginx/conf.d/default.conf
 
-EXPOSE 3000
+EXPOSE 80
 
 CMD ["nginx", "-g", "daemon off;"]
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@
 docker pull $1
 docker stop devops-container || true
 docker rm devops-container || true
-docker run -d -p 3000:3000 --name devops-container $1
+docker run -d -p 80:80 --name devops-container $1
